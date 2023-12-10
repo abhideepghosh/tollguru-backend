186cf809f4151bdac06a5419c1a328014abff688
Abhideep Ghosh <109783702+abhideepghosh@users.noreply.github.com>
2023-12-10 18:36:45 +0530

    Backend-V1

---
+++ server.js
@@ -0,0 +1,45 @@
+import cors from "cors";
+import axios from "axios";
+import express from "express";
+
+const app = express();
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.post("/", (req, res) => {
+  axios
+    .post(
+      "https://apis.tollguru.com/toll/v2/origin-destination-waypoints/",
+      req.body,
+      {
+        headers: {
+          "x-api-key": "J69frFMBFthmPTH7HTTm7NgF7p4dFRNf",
+          "Content-Type": "application/json",
+        },
+      }
+    )
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("Something went wrong");
+    });
+});
+
+app.listen(port, () => {
+  console.log("Started!");
+});
